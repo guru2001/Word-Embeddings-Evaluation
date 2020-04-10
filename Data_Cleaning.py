27b2cee1876fb0ea3d483e14d905cfc89b1ac29c
damasravni19 <damasravani19@gmail.com>
2020-04-10 22:48:25 +0530

    Data cleaning nearly Done

--- Data_Cleaning.py
+++ Data_Cleaning.py
@@ -1,10 +1,59 @@
 import re
-f = open("sample1.txt","r")
+f = open("Stemmedoutput1.txt","r")
 data = f.read()
-f.close()
+# print(data)
+# Lines=data.split('.')
+# print(Lines)
+# print("Done")
+# print(len(Lines))
+#( Inferiority Complex ) to remove these tyor of characters
 data = re.sub(r'\([^)]*\)', '', data)
-data = re.sub('^[a-zA-Z0-9]+$','',data)
+#alpha numeric characters
+data = re.sub('[a-zA-Z]+','',data) 
+#
 data = re.sub('●','',data)
+#Replacing '.' with a new line character
+data = re.sub(r'\n+', '\n', data)
+#replacing number of white spaces with single white space
+data=re.sub(' +',' ',data)
+#correcting name with initials 
 
-f = open("samplecleaned.txt", "w") 
-f.write(data)
+# for line in Lines: 
+#     print(line)
+
+f1 = open("temp.txt", "w") 
+f1.write(data)
+
+f2=open("temp.txt","r")
+f3=open("Stem&cleaned1.txt","w")
+data=f2.read()
+Lines=data.split('\n')
+i=0
+for line in Lines :
+    # print(i)
+    i=i+1
+    # line= re.sub(r'\.','',line)
+    # line= re.sub(r'\|','',line)
+    # line= re.sub(r'\*','',line)
+    # line= re.sub(r'\[','',line)
+    # line= re.sub(r'\]','',line)
+    # line= re.sub(r'\\','',line)
+    # line= re.sub(r'\/','',line)
+    # line= re.sub(r'\-','',line)
+    # line= re.sub(r'\#','',line)
+    #removing unnecessary characters
+    line= re.sub(r'  +','',line)
+    line= re.sub(r'[\.\|\*\[\]\\\/\-\#\)\(\&\;]','',line)
+
+    # print(line.split())
+    if (len(line.split())>0) :
+        x=line.split()
+        y=str(x[len(x)-1])
+        if len(re.sub(r'[^\w\s]','', y))==1 :
+            line=line+'.'
+            f3.write(line)
+        else :
+            f3.write(line+'\n')
+
+
+
